92026f275fd5fa353d2ebf25aa4ffef8413f985c
rakeshcooper <rakeshmurali097@gmail.com>
2024-12-23 19:05:54 +0530

    need to fix JWT error

--- server.js
+++ server.js
@@ -2,8 +2,13 @@ import express from "express"
 import dotenv  from 'dotenv'
 dotenv.config()
 import jwt from "jsonwebtoken"
+import bodyParser from "body-parser"
 
 const app = express()
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }));
+
+console.log(jwt);
 
 
 const posts = [
@@ -17,20 +22,35 @@ const posts = [
     }  
 ]
 
-app.use(express.json())
 
-app.get('/posts', (req,res) => {
-    res.json(posts)
+
+app.get('/posts',authenticateToken, (req,res,next) => {
+    res.json(posts.filter(post => post.username === req.user.name))
 })
 
-app.post('/login', (req,res) => {
-    //Authentication Username
-    const username = req.body.username
-    const user = {name: username}
+// app.post('/login', (req,res) => {
+//     //Authentication Username
+//     const username = req.body.username
+//     const user = {name: username}
 
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-    res.json( {accessToken: accessToken} )
-})
+//     const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+//     res.json( {accessToken: accessToken} )
+// })
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token === null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET,(err, user) => {
+        console.log(err);
+        if(err) return res.sendStatus(403)
+        req.user = user
+        next()
+    })    
+
+         
+}
 
 app.listen(5000, () => {
     console.log(`The PORT of ${5000} is running`)   
